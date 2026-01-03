a0075056ee601039256a8e7f419684991b92a310
SNA Rahanet <navidjan.azimi132@gmail.com>
2026-01-03 14:46:06 +0430

    storing data to metadata is added

--- src/decorators/class.decorator.ts
+++ src/decorators/class.decorator.ts
@@ -1,3 +1,5 @@
+import 'reflect-metadata'
+
 // Example of a simple class decorator that logs when a class is created
 function simpleLogger(constructor: Function) {
     console.log(`Class ${constructor.name} has been created.`);
@@ -41,6 +43,7 @@ function logMethod() {
             console.log(end)
 
             console.log(`Method ${propertyKey} executed in ${end - start} ms`);
+            return result;
         }
     }
 }
@@ -54,3 +57,52 @@ class Caclulator {
 
 const calc = new Caclulator();
 calc.add(2, 3);
+
+
+export const ROLE_KEY = 'role';
+
+export function Role(role: string) {
+    return function (
+        target: Object,
+        propertyKey: string,
+    ) {
+        Reflect.defineMetadata(ROLE_KEY, role, target, propertyKey);
+    }
+}
+
+
+class SecureCalculator {
+    @Role('admin')
+    add(a: number, b: number): number {
+        return a + b;
+    }
+
+    @Role('user')
+    subtract(a: number, b: number): number {
+        return a - b;
+    }
+
+    @Role('admin')
+    multiply(a: number, b: number): number {
+        return a * b;
+    }
+}
+
+const role = Reflect.getMetadata('role', SecureCalculator.prototype, 'add');
+console.log(`Role required to access add method: ${role}`);
+
+
+function scanClasses(target: any) {
+    const proto = target.prototype;
+
+    Object.getOwnPropertyNames(proto).forEach(name => {
+        const role = Reflect.getMetadata(ROLE_KEY, proto, name);
+        if (role) {
+            console.log(`Method ${name} requires role: ${role}`)
+        }
+
+    })
+}
+
+
+scanClasses(SecureCalculator)
